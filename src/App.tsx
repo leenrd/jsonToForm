5961b553be1e9597f543637c4b4117a29a2da6e1
Leenard <lolleenard0@gmail.com>
2024-07-07 15:42:54 +0800

    feat: tremor react

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+    <main className="grid grid-cols-4 text-white p-7">
+      <section className="col-span-1">
+        <h1 className="font-bold text-4xl">JsonToForm</h1>
+      </section>
+
+      {/* form display */}
+      <section className="col-span-3">
+        <p className="text-lg">
+          <span className="bg-orange-500 rounded-sm text-orange-500">---</span>{" "}
+          is an exploratory project that uses the factory design pattern for
+          React. It provides a set of consistent and elegant conditional logic
+          to components, which are designed to instantiate different form
+          component formats based on a json array of props.
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      </section>
+    </main>
+  );
 }
 
-export default App
+export default App;
